5379de53251b040d8285a9dfa8aecd118311cd46
squeletteur <antoinemulliez@orange.fr>
2019-04-11 11:44:07 +0200

    mouvements basiques des ennemis

---
+++ Fly Hunter/Assets/Scripts/Ennemys/EnnemyBasicsMovements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyBasicMovements : MonoBehaviour
+{
+
+    public float speed;
+    private float step;
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
