2cdc9e67093f12ade98edcd3252cfdbe656ba234
crazyk1k1 <crazyk1k1@hotmail.com>
2019-04-16 11:09:12 +0100

    push

--- Fly Hunter/Assets/Scripts/Ennemys/EnnemyBasicsMovements.cs
+++ Fly Hunter/Assets/Scripts/Ennemys/EnnemyBasicsMovements.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnnemyBasicsMovements : MonoBehaviour
 {
+    public Collider tapette;
+    public Rigidbody fly;
+    public bool active;
 
     public float speed;
     private float step;
@@ -18,7 +21,48 @@ public class EnnemyBasicsMovements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if(active)
+        {
+            step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+        
+
+
     }
+    /*
+    void OnCollisionEnter(Collision col)
+    {
+        active = false;
+
+        if (col.gameObject.name == "haut")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    */
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("sa marche");
+
+        if (collision.gameObject.CompareTag("weapon"))
+        {
+            active = false;
+
+            Debug.Log("sa marche");
+
+            fly.useGravity = true;
+            fly.isKinematic = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("sa marche");
+
+    }
+
+
+
 }
