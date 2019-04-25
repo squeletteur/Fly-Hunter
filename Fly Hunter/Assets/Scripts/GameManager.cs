b9f8eccbcd1e3c8c7546de7261e6ca00b6d9fb4f
Schwindowsky_Yann <yann.schwindowsky@gmail.com>
2019-04-25 14:20:40 +0200

    Import Interaction fenêtre vers Scene yannTest

---
+++ Fly Hunter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 500;
+    public bool finVague;
+    int vague = 1;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Fly Hunter/Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject TextScore;
+    public GameObject GM;
+    private Text myText;
+    private GameManager GMs;
+
+    // Use this for initialization
+    void Start()
+    {
+        myText = TextScore.GetComponent<Text>();
+        GMs = GM.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EditTS();
+    }
+
+    void EditTS()
+    {
+        string example = GMs.score.ToString();
+        myText.text = "Score : " + example;
+        myText.color = Color.black;
+    }
+}
