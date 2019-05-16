68c91b4de220e0b6adcb31ee291412ff49eba728
crazyk1k1 <crazyk1k1@hotmail.com>
2019-05-16 15:51:26 +0100

    modif armes en mieux

---
+++ Fly Hunter/Assets/Scripts/weapon/dispawnObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dispawnObject : MonoBehaviour {
+
+    float timer = 0;
+    public float MaxTime = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= MaxTime)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
