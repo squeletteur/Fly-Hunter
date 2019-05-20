51f31a7830a40492b9d69893b3718f4b740e8d0b
Ritshy <raphaelbureau09@gmail.com>
2019-05-20 15:51:54 +0200

    gamemanager changements

--- Fly Hunter/Assets/Scripts/GameManager.cs
+++ Fly Hunter/Assets/Scripts/GameManager.cs
@@ -400,7 +400,7 @@ public class GameManager : MonoBehaviour
         vie += bonusHealth;
         wave++;
         interWave = false;
-        ShowingWaveNumber(wave, waveNumber, WaveNumberText);
+        //ShowingWaveNumber(wave, waveNumber, WaveNumberText);
         SpawnTrophys(parentTrophys);
     }
 
