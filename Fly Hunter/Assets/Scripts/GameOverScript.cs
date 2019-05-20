bd8f0f58abfc4d75a6fa2458a0e978e825f43d6e
Schwindowsky_Yann <yann.schwindowsky@gmail.com>
2019-05-20 13:24:08 +0200

    Game Over Fini + Suppr .meta wtf

--- Fly Hunter/Assets/Scripts/GameOverScript.cs
+++ Fly Hunter/Assets/Scripts/GameOverScript.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverScript : MonoBehaviour
 {
+    private float timerFondu;
+    public float IncrTimerFondu = 0.005f;
+
     public GameManager GM;
+
+    [Header("Visuels")]
+    public GameObject TextGO;
+    public Image ImageFonduNoir;
 
 	// Use this for initialization
 	void Start ()
@@ -16,13 +24,15 @@ public class GameOverScript : MonoBehaviour
 	void Update ()
     {
         GameOver();
+        timerFondu += IncrTimerFondu * Time.deltaTime;
 	}
 
     private void GameOver()
     {
         if(GM.vie <= 0)
         {
-
+            TextGO.SetActive(true);
+            ImageFonduNoir.fillAmount = timerFondu;
         }
     }
 }
