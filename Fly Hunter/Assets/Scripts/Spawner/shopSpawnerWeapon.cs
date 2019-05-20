f67caef4b34eab7fd4c52bb4700a8d5078824bfe
crazyk1k1 <crazyk1k1@hotmail.com>
2019-05-20 10:37:21 +0100

    sang + debut boutique + ui

---
+++ Fly Hunter/Assets/Scripts/Spawner/shopSpawnerWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shopSpawnerWeapon : MonoBehaviour {
+
+    public GameObject weapon;
+    public int price;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("laserAchat") && GameManager.Singleton.score >= price)
+        {
+            Instantiate(weapon, transform.position, transform.rotation);
+            GameManager.Singleton.score -= price;
+        }
+    }
+}
