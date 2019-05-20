9ad175160a872b9b6f0bd7d438fb8d7aaf5c6d29
Schwindowsky_Yann <yann.schwindowsky@gmail.com>
2019-05-20 11:44:48 +0200

    WIP Game Over Script

---
+++ Fly Hunter/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+    public GameManager GM;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameOver();
+	}
+
+    private void GameOver()
+    {
+        if(GM.vie <= 0)
+        {
+
+        }
+    }
+}
